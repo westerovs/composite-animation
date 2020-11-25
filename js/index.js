cc86721b46fa89545057477bccb0aed603240cbb
Alexander_LIVE <aleksandr.vesterovskiy@neuro-city.ru>
2020-11-25 19:08:45 +0300

    all okay

--- js/index.js
+++ js/index.js
@@ -1,17 +1,36 @@
-const container = document.querySelector('.search-logo-active')
-const containerSpring = document.querySelector('.container-spring')
+const containerNormal = document.querySelector('.search-logo-normal')
+const containerActive = document.querySelector('.search-logo-active')
+const containerActiveSpring = document.querySelector('.container-active-spring')
 
-container.addEventListener(('animationend'), () => {
-    container.style.transform = 'rotate(0)';
+function runSearchActive() {
+    containerActive.style.display = 'block'
+    containerActiveSpring.style.display = 'block'
 
-    container.addEventListener(('transitionend'), () => {
-        containerSpring.style.height = '192px';
-
-        containerSpring.addEventListener(('transitionend'), () => {
-            containerSpring.style.animationName = 'spring-color'
-            document.querySelector('.search-logo-active__text').style.animationName = 'spring-color-text'
-            document.querySelector('.search-logo-active__text').style.display = 'block'
-        })
+    containerActive.addEventListener(('animationend'), () => {
+        containerActive.style.transform = 'rotate(0)';
     })
+
+    containerActive.addEventListener(('transitionend'), () => {
+        containerActiveSpring.style.height = '192px';
+    })
+
+    containerActiveSpring.addEventListener(('transitionend'), () => {
+        containerActiveSpring.style.animationName = 'spring-color'
+        document.querySelector('.search-logo-active__text').style.animationName = 'spring-color-text'
+        document.querySelector('.search-logo-active__text').style.display = 'block'
+    })
+}
+
+
+containerNormal.addEventListener('pointerdown', () => {
+    containerNormal.style.top = '450px'
+    containerNormal.style.left = '584px'
+    containerNormal.style.transform = 'scale(0.8)'
+    containerNormal.style.animationIterationCount = 'revert'
 })
 
+containerNormal.addEventListener('transitionend', () => {
+    runSearchActive()
+    containerNormal.style.transform = 'scale(1)'
+    containerNormal.style.display = 'none'
+})
